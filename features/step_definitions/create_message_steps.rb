12d9fe4e83aeec45bbef9dd407f5068d86004943
junydania <o.dania@aol.com>
2017-05-05 01:01:19 +0200

    refactored the create message test

--- features/step_definitions/create_message_steps.rb
+++ features/step_definitions/create_message_steps.rb
@@ -2,3 +2,9 @@
 Then(/^I select "([^"]*)" from "([^"]*)" list$/) do |recipient, recipients|
   select recipient, from: recipients
 end
+
+Given(/^the following users exists$/) do |table|
+  table.hashes.each do |hash|
+    User.create!(hash)
+  end
+end
