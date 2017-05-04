5f07b9d38a8ce7f121bae03ec9d35017f31d80be
claes <claesca@outlook.com>
2017-05-04 10:17:15 +0200

    add cucumber steps and tests are passing

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -7,10 +7,23 @@ Given(/^I click on "([^"]*)" link$/) do |link|
   click_link link
 end
 
+Given(/^I fill in field "([^"]*)" with "([^"]*)"$/) do |field, content|
+  fill_in(field, with: content)
+end
+
+Given(/^I click on "([^"]*)" button$/) do |button|
+  click_button(button)
+end
+
+Then(/^I should expect page to have content "([^"]*)"$/) do |content|
+  expect(page).to have_content(content)
+end
+
+
 Then(/^I should see field "([^"]*)" of type "([^"]*)"$/) do |field, type|
-  page.should have_selector ('input[type=' + type + ']#' + field)
+  expect(page).to have_selector ('input[type=' + type + ']#' + field)
 end
 
 Then(/^I should a see "([^"]*)" button$/) do |button|
-  page.should have_selector ('input[name=' + button + ']')
+  expect(page).to have_selector ('input[name=' + button + ']')
 end
