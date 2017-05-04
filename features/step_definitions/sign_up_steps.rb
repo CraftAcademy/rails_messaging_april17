9c2ca51a547f6e0af705e4558a4909efddc65d45
claes <claesca@outlook.com>
2017-05-04 15:17:53 +0200

    refactor of the sad path checks for second user

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -1,6 +1,16 @@
+def page_path(page_name)
+  case page_name
+  when 'Home' then
+    root_path
+  when 'Sign up' then
+    new_user_registration_path
+  else
+    raise "#{page_name} page path is not defined"
+  end
+end
 
-Given(/^I visit the "([^"]*)" page$/) do |page|
-  visit page
+Given(/^I visit the "([^"]*)" page$/) do |page_name|
+  visit page_path(page_name)
 end
 
 Given(/^I click on "([^"]*)" link$/) do |link|
@@ -26,4 +36,15 @@ end
 
 Then(/^I should a see "([^"]*)" button$/) do |button|
   expect(page).to have_selector ('input[name=' + button + ']')
+end
+
+Given(/^I signup as "([^"]*)" with email "([^"]*)" and password "([^"]*)"$/) do |name, email, password|
+  steps %{
+    Given I visit the "Sign up" page
+    And I fill in field "user_name" with "#{name}"
+    And I fill in field "user_email" with "#{email}"
+    And I fill in field "user_password" with "#{password}"
+    And I fill in field "user_password_confirmation" with "#{password}"
+    And I click on "Create" button
+  }
 end
